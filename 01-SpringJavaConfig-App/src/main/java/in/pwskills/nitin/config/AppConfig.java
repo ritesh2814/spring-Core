520a01465ea9bbd5c35655ced13e856efab4388f
ritesh2814 <ritesh282017@gmail.com>
2024-09-05 12:39:17 +0530

    Code of spring core

---
+++ 01-SpringJavaConfig-App/src/main/java/in/pwskills/nitin/config/AppConfig.java
@@ -0,0 +1,29 @@
+package in.pwskills.nitin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import in.pwskills.nitin.bean.Employee;
+
+@Configuration
+public class AppConfig {
+	static {
+		System.out.println("AppConfig.class file is loading...");
+	}
+
+	public AppConfig() {
+		System.out.println("AppConfig object is created...");
+	}
+
+	@Bean(name = "emp")
+	public Employee empObj() {
+		System.out.println("AppConfig.empObj():: Creating the object...");
+		Employee employee = new Employee();
+		employee.setEid(10);
+		employee.setEname("sachin");
+		employee.setEage(52);
+		employee.setEaddress("MI");
+		return employee;
+	}
+
+}
